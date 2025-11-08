efd6ff72fb81de81408b29d3f2ca17f37375bdf7
Trikkitt <mjt@mjturner.co.uk>
2025-11-08 18:16:31 +0000

    Add files via upload

---
+++ OrbCode.py
@@ -0,0 +1,143 @@
+import network
+import espnow
+import OrbFunctions
+import uos
+import time
+from machine import deepsleep,UART,reset
+
+#uos.dupterm(None,1)
+uart = UART(0,115200,bits=8, parity=None, stop=1, timeout=2, timeout_char=2, rxbuf=255)
+
+CodeVersionHigh=1
+CodeVersionLow=0
+PICVersionHigh=0
+PICVersionLow=0
+VersionChange=True
+
+sta = network.WLAN(network.WLAN.IF_STA)
+sta.active(True)
+sta.disconnect() 
+sta.config(channel=1)
+e = espnow.ESPNow()
+e.active(True)
+
+
+
+gamehost=OrbFunctions.DiscoverHost(e,sta,uart)
+AliveInterval=30
+LastAliveMsg=time.time()
+
+while True:
+    if VersionChange==True:
+        VersionChange=False
+        msg=bytearray(b'\xfe\x07')
+        msg.append(CodeVersionHigh)
+        msg.append(CodeVersionLow)
+        msg.append(OrbFunctions.GetVersionHigh())
+        msg.append(OrbFunctions.GetVersionLow())
+        msg.append(PICVersionHigh)
+        msg.append(PICVersionLow)
+        msg.append(0)
+        crc=OrbFunctions.crc16(msg)
+        msg.append(crc & 255)
+        msg.append(crc >> 8)
+        e.send(gamehost,msg)
+        
+    if time.time()>LastAliveMsg:
+        LastAliveMsg=time.time() + AliveInterval
+        awakemsg=bytearray(b'\xfe\x06')
+        awakemsg.extend(sta.config('mac'))
+        crc=OrbFunctions.crc16(awakemsg)
+        awakemsg.append(crc & 255)
+        awakemsg.append(crc >> 8)
+        e.send(gamehost,awakemsg)
+        
+    if (uart.any()):
+        cmd=uart.read(10)
+        e.send(gamehost,cmd)
+        #if OrbFunctions.crc16(cmd)==0:
+        if cmd[0]<101: # Message for server
+            e.send(gamehost,cmd)
+        else:
+            Acknowledge=False
+            if cmd[0]==0xCD:
+                PICVersionHigh=cmd[1]
+                PICVersionLow=cmd[2]
+                VersionChange=True
+                Acknowledge=True
+            if Acknowledge==True:
+                reply=bytearray(b'\xff\x00\x00\x00\x00\x00\x00')
+                reply.extend(cmd[0])
+                crc=OrbFunctions.crc16(reply)
+                reply.append(crc >> 8)
+                reply.append(crc & 255)
+                uart.write(reply)
+                
+    if (e.any()):
+        host,msg=e.irecv()
+        if len(msg)>1:
+            if OrbFunctions.crc16(msg)==0:
+                if msg[0]<101:
+                    #print(msg)
+                    uart.write(msg)
+                else:
+                    if msg[0]==0xFA:
+                        FailureReason=0
+                        if OrbFunctions.connectwifi()==True:
+                            if msg[1]==101:
+                                filename='OrbCodeReborn.hex'
+                            if msg[1]==1:
+                                filename='OrbCode.py'
+                            if msg[1]==2:
+                                filename='OrbFunctions.py'
+                            if OrbFunctions.downloadfile(filename)==True:
+                                import os
+                                newfilename="new-" + filename
+                                oldfilename="old-" + filename
+                                filestat=os.stat(newfilename)
+                                targetsize=(msg[4]*65536) + (msg[3] * 256) + msg[2]
+                                if filestat[6]==targetsize:
+                                    try:
+                                        os.remove(oldfilename)
+                                    except:
+                                        a=2
+                                    try:
+                                        os.rename(filename,oldfilename)
+                                        os.rename(newfilename,filename)
+                                        reply=bytearray(b'\xfa')
+                                        reply.append(msg[1])
+                                        reply.extend(b'\x00\x00\x00\x00\x00\x00')
+                                        crc=OrbFunctions.crc16(reply)
+                                        reply.append(crc >> 8)
+                                        reply.append(crc & 255)
+                                        e.send(gamehost,reply)
+                                        uart.write(reply)
+                                    except:
+                                        FailureReason=4
+                                    
+                                else:
+                                    FailureReason=3
+                            else:
+                                FailureReason=2
+                        else:
+                            FailureReason=1
+                        if FailureReason>0: 
+                            sta.disconnect()
+                            sta.config(channel=1)
+                            reply=bytearray(b'\xfe\x08')
+                            reply.append(FailureReason)
+                            reply.extend('\x00\x00\x00\x00\x00')
+                            crc=OrbFunctions.crc16(reply)
+                            reply.append(crc & 255)
+                            reply.append(crc >> 8)
+                            e.send(gamehost,reply)
+                            
+                            
+                    if msg[0]==1:
+                        reply=bytearray(b'\xff\x02\x00\x00\x00\x00\x00\x00')
+                        crc=OrbFunctions.crc16(reply)
+                        reply.append(crc & 255)
+                        reply.append(crc >> 8)
+                        e.send(gamehost,reply)
+
+                
---
+++ OrbFunctions.py
@@ -0,0 +1,112 @@
+def GetVersionHigh():
+    return 1
+
+def GetVersionLow():
+    return 0
+
+
+def get_config():
+    import json
+    try:
+        configfile=open("config.txt","r")
+        cfgdict=json.load(configfile)
+        configfile.close()
+    except BaseException as e:
+        cfgdict={}
+    return cfgdict
+
+
+def save_config(cfgdict):
+    import json
+    savesuccess=False
+    try:
+        cfgtxt=json.dumps(cfgdict)
+        configfile=open("config.txt","w")
+        configfile.write(cfgtxt)
+        configfile.close()
+        savesuccess=True
+    except:
+        savesuccess=False
+
+
+def crc16(data):
+    crc = 0xFFFF 
+    for byte in data:
+        crc = crc ^ byte
+        for _ in range(8):
+            if (crc & 0x01)==0:
+                crc=crc >> 1
+            else:
+                crc=crc >> 1
+                crc = crc ^ 0xA001
+            crc &= 0xFFFF
+    return crc
+
+def connectwifi():
+    import network
+    import utime
+    wifi_SSID='TheTardisGuest'
+    wifi_PSK='Trikkitt'
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect(wifi_SSID,wifi_PSK)
+
+    while not sta_if.isconnected():
+    	utime.sleep(2)
+
+def downloadfile(filename):
+    import os
+    newfilename='new-' + filename
+    try:
+        os.remove(newfilename)
+    except:
+        a=1
+    try:
+        import requests
+        url='https://raw.githubusercontent.com/Trikkitt/OrbsReborn/refs/heads/main/' + filename
+        #r=requests.get(url)
+        #open(filename,'wb').write(r.content)
+        r=requests.get(url)
+        data=r.text
+        f=open(newfilename,'wb')
+        f.write(data)
+        f.close()
+        r.close()
+        filestat=os.stat(newfilename)
+        if filestat[6]==14:
+            return False
+        else:
+            return True
+    except:
+        return False
+
+
+def DiscoverHost(pe,psta,puart):
+    bcast = b'\xff' * 6
+    mygamehost=b'\xff' * 6
+    discoveryPacket=bytearray(b'\xfe\x05')
+    discoveryPacket.extend(psta.config('mac'))
+    crc=crc16(discoveryPacket)
+    discoveryPacket.append(crc & 255)
+    discoveryPacket.append(crc >> 8)
+
+    while bcast==mygamehost:
+        pe.send(bcast,discoveryPacket)
+        host, msg=pe.irecv(2000)
+        if not host:
+            picmsg=bytearray(b'\xfe\x00\x00\x00\x00\x00\x00\x00')
+            crc=crc16(picmsg)
+            picmsg.append(crc & 255)
+            picmsg.append(crc >> 8)
+            puart.write(picmsg)
+        else:
+            if len(msg)==10:
+                crc=crc16(msg)
+                if crc==0 and msg[0]==0xfb and msg[1]==0x05:
+                    mygamehost=msg[2:8]
+    picmsg=bytearray(b'\xfb\x00\x00\x00\x00\x00\x00\x00')
+    crc=crc16(picmsg)
+    picmsg.append(crc & 255)
+    picmsg.append(crc >> 8)
+    puart.write(picmsg)
+    return mygamehost
